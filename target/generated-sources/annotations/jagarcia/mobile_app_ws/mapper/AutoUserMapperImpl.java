e142c0cb966398bc4b5b7dc014fa2b89a7c9c7fa
Alexis Martinez <jhonyalexis100@gmail.com>
2024-07-12 09:02:46 -0500

    finish

--- target/generated-sources/annotations/jagarcia/mobile_app_ws/mapper/AutoUserMapperImpl.java
+++ target/generated-sources/annotations/jagarcia/mobile_app_ws/mapper/AutoUserMapperImpl.java
@@ -6,8 +6,8 @@ import javax.annotation.processing.Generated;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2024-06-17T18:09:57-0500",
-    comments = "version: 1.5.5.Final, compiler: javac, environment: Java 21.0.2 (Oracle Corporation)"
+    date = "2024-07-11T21:07:41-0500",
+    comments = "version: 1.5.5.Final, compiler: javac, environment: Java 22.0.1 (Oracle Corporation)"
 )
 public class AutoUserMapperImpl implements AutoUserMapper {
 
